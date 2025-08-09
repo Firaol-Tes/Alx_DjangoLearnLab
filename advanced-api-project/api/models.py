e7fdb08c89d5b4e8cd01c6b5c877c44b12128263
Mosisa Feyissa <mosisafaye@gmail.com>
2025-08-09 16:19:18 +0300

    advanced_api

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+"""
+Models:
+Author - stores basic info like name.
+Book - stores book info and links to an Author. One Author can have many Books.
+"""
